1667472ac00df896a24dc9754d368e2789f6a8d7
Andres-OM <andresito.aom@gmail.com>
2025-05-28 19:21:09 +0200

    colores

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,8 +32,11 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->favicon(asset('images/favicon.ico'))
             ->colors([
-                'primary' => Color::Amber,
+                //'primary' => Color::Amber,
+                'primary' => '#FCA311',
             ])
+            ->font('Roboto')
+            ->darkMode(false)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
